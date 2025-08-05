19a48f05b5ca018af2f4ed1ad8d359936c54557f
Swasti-jain16 <swastij166@gmail.com>
2025-08-05 19:02:27 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import javax.sound.midi.SysexMessage;
+
  public class Arithmetic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,4 +20,5 @@ import javax.sound.midi.SysexMessage;
 }
 
     
+
 
