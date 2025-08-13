09407647068ccb48fa3d380b0821b86c6ce4f6ce
Swasti-jain16 <swastij166@gmail.com>
2025-08-13 18:48:52 +0530

    Add program to generate alphabetical pyramids
    The program uses nested loops to achieve the following :
    - The outer loop iterates through the rows.
    - The inner loop prints the letters for each row, incrementing the character from 'A' up to the row number.

---
+++ AlphabetPyramid.java
@@ -0,0 +1,17 @@
+public class AlphabetPyramid {
+    public static void main (String[] args) {
+        int rows = 5;
+        char ch = 'A';
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+            ch = 'A';
+            
+        }
+    }
+    
+}
